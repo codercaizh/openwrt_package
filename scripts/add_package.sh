d06ee3aaa4bce6cbdd00ddfa633918c29b2e09bf
codercaizh <545347837@qq.com>
2022-11-26 16:36:21 +0800

    去掉node依赖

--- scripts/add_package.sh
+++ scripts/add_package.sh
@@ -5,3 +5,4 @@ git clone --depth=1 https://github.com/ntop/n2n.git /tmp/n2n
 mv /tmp/n2n/packages/openwrt ./
 rm -rf /tmp/n2n
 rm -rf /root/lede/package/lean/n2n
+sed -i 's/=+n2n/=+n2n-edge/g' feeds/luci/luci-app-n2n/Makefile
