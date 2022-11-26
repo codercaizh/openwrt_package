2bd7a1dfce1d9e8ea3b68d32a7f386eda8466edd
codercaizh <545347837@qq.com>
2022-11-26 11:16:01 +0800

    添加add_package脚本

---
+++ scripts/add_package.sh
@@ -0,0 +1,7 @@
+# 执行脚本的目录在openwrt/package
+
+## 替换n2n为开发版
+git clone --depth=1 https://github.com/ntop/n2n.git /tmp/n2n
+mv /tmp/n2n/packages/openwrt ./
+rm -rf /tmp/n2n
+rm -rf /root/lede/package/lean/n2n
