12507239325c9e6fc96d40ebf8a21238a6a2b4b0
codercaizh <545347837@qq.com>
2022-11-27 02:21:38 +0800

    修复脚本

--- scripts/after_update_feeds.sh
+++ scripts/after_update_feeds.sh
@@ -1,5 +1,5 @@
 # 执行脚本的目录在openwrt
 ## 更换n2n版本
-rm -rf lean/n2n/patches
+rm -rf package/lean/n2n/patches
 sed -i 's/PKG_VERSION:=3.0/PKG_VERSION:=3.1.1/g' package/lean/n2n/Makefile
 sed -i 's/^PKG_HASH:.*/PKG_HASH:=skip/g' package/lean/n2n/Makefile
