51a9c7c85e378b2c716a9b19aa00c845ab4900a8
codercaizh <545347837@qq.com>
2022-11-27 02:20:16 +0800

    优化脚本

--- scripts/after_update_feeds.sh
+++ scripts/after_update_feeds.sh
@@ -1,5 +1,4 @@
-# 执行脚本的目录在openwrt/package
-
+# 执行脚本的目录在openwrt
 ## 更换n2n版本
 rm -rf lean/n2n/patches
 sed -i 's/PKG_VERSION:=3.0/PKG_VERSION:=3.1.1/g' package/lean/n2n/Makefile
--- scripts/before_update_feeds.sh
+++ scripts/before_update_feeds.sh
@@ -1,2 +1,3 @@
+# 执行脚本的目录在openwrt
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
