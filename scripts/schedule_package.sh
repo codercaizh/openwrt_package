48a8685af3450ae0beb9f7b131cdd3211525402b
codercaizh <545347837@qq.com>
2022-12-02 00:24:35 +0800

    修改推送时间内容

--- scripts/schedule_package.sh
+++ scripts/schedule_package.sh
@@ -18,6 +18,7 @@ NAME=schedule_package_$NOW_DATE
 # 推送编译通知到手机上，可以自己到pushplus申请token配到环境中
 START_CONTENT='http://www.pushplus.plus/send?token='${PUSH_TOKEN}'&title=%E5%BC%80%E5%A7%8B%E7%BC%96%E8%AF%91openwrt%E5%9B%BA%E4%BB%B6&content=%E6%9C%AC%E6%AC%A1%E7%BC%96%E8%AF%91%E5%AE%B9%E5%99%A8%E5%90%8D%EF%BC%9A'$NAME
 curl $START_CONTENT
+START_TIME=`date +%Y-%m-%d_%H:%M:%S`
 cd $BASE_DIR
 git pull
 rm -rf $BASE_DIR/openwrt_build_tmp
@@ -31,5 +32,6 @@ find $FIRMWARE_DIR -mtime +$FIRMWARE_EXPIRED_DAY  -exec rm {} \;
 mkdir -p $FIRMWARE_OUTPUT_DIR && mv $BASE_DIR/openwrt_build_tmp/artifact/* $FIRMWARE_OUTPUT_DIR
 [ `docker ps -a | grep $NAME | wc -l` -eq 0 ] || docker rm -f $NAME
 echo '固件定时编译完毕：'$FIRMWARE_OUTPUT_DIR
-END_CONTENT='http://www.pushplus.plus/send?token='${PUSH_TOKEN}'&title=%E5%9B%BA%E4%BB%B6%E7%BC%96%E8%AF%91%E5%AE%8C%E6%88%90&content=%E5%9B%BA%E4%BB%B6%E6%89%80%E5%9C%A8%E7%9B%AE%E5%BD%95%EF%BC%9Ahttps%3A%2F%2Ffile.4yuan.top%2Ffiles%2F'$NOW_DATE
+END_TIME=`date +%Y-%m-%d_%H:%M:%S`
+END_CONTENT='http://www.pushplus.plus/send?token='${PUSH_TOKEN}'&title=openwrt%E5%9B%BA%E4%BB%B6%E7%BC%96%E8%AF%91%E5%AE%8C%E6%88%90&content=openwrt%E5%9B%BA%E4%BB%B6%E6%89%80%E5%9C%A8%E7%9B%AE%E5%BD%95%EF%BC%9A'$NOW_DATE'%EF%BC%8C%E7%BC%96%E8%AF%91%E5%BC%80%E5%A7%8B%E6%97%B6%E9%97%B4%EF%BC%9A'$START_TIME'%EF%BC%8C%E7%BC%96%E8%AF%91%E5%AE%8C%E6%88%90%E6%97%B6%E9%97%B4%EF%BC%9A'$END_TIME
 curl $END_CONTENT
