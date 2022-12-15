cf1a179f3ee4f4090564eec24969fd47e7964c0c
codercaizh <545347837@qq.com>
2022-12-15 21:21:39 +0800

    修改检查是否编译成功逻辑

--- scripts/build_with_docker.sh
+++ scripts/build_with_docker.sh
@@ -11,11 +11,26 @@ DEVICE=$1
 CONFIG=$2
 ONLY_PACKAGE=$3
 OUTPUT_DIR=$ARTIFACT_DIR/$DEVICE
-if [ "$CONFIG" == "armv8" ];then
-    BUILD_TARGET_DIR=$OPENWRT_DIR/bin/targets/armvirt/64
-else
-    BUILD_TARGET_DIR=$OPENWRT_DIR/bin/targets/ramips
-fi
+IS_COMPLIE=0
+check_complie_status() {
+    COMPLIE_CONFIG=$1
+    if [ "$COMPLIE_CONFIG" == "armv8" ];then
+        if ls $OPENWRT_DIR/bin/targets/armvirt/64/openwrt-armvirt-64-default-rootfs.tar.gz 1> /dev/null 2>&1; then
+            echo "ARM盒子固件已存在"
+            IS_COMPLIE=1
+        else
+            IS_COMPLIE=0
+        fi
+    else
+        if ls $OPENWRT_DIR/bin/targets/ramips/*/*.bin 1> /dev/null 2>&1; then
+            echo "硬路由固件已存在"
+            IS_COMPLIE=1
+        else
+            IS_COMPLIE=0
+        fi
+    fi
+}
+
 source $SCRIPT_DIR/package_firmware.sh
 if [ ! -d "$OPENWRT_DIR/.git" ]; then
     echo '未找到openwrt源码，正在检出源码'
@@ -38,13 +53,13 @@ if [ "$DEVICE" == "0" ];then
     cp .config $CONFIG_DIR/$CONFIG.config
     exit 0
 fi
-
+check_complie_status
 if test -z "$ONLY_PACKAGE";then
     echo '仅打包选项未开启，进入编译流程'
 else
     # 当开启ONLY_PACKAGE选项，并且底包确实已存在，则跳过编译，直接进入打包
     echo '检测到开启仅打包选项'
-    if [ -d "$BUILD_TARGET_DIR" ]; then
+    if [ "$IS_COMPLIE" == "1" ]; then
         SKIP_BUILD=1
         echo '当前编译产物目录已存在'
     else
@@ -58,21 +73,22 @@ if test -z "$SKIP_BUILD";then
     make download -j`nproc` || make download -j1
     echo '编译依赖下载完毕'
     rm -rf $OPENWRT_DIR/bin
-    echo '开始编译底包'
+    echo '开始编译'
     make -j`nproc`
 else
-    echo '跳过编译底包流程'
+    echo '跳过编译流程'
 fi
 
 # 检测编译是否成功
-
-if [ ! -d "$BUILD_TARGET_DIR" ]; then
+check_complie_status
+ if [ "$IS_COMPLIE" == "0" ]; then
     echo '编译失败，即将使用单线程重试编译'
     make V=s -j1
 else
     echo '编译完毕'
 fi
-if [ ! -d "$BUILD_TARGET_DIR" ]; then
+check_complie_status
+if [ "$IS_COMPLIE" == "0" ]; then
     echo '编译失败，请根据日志排查原因'
     exit -1
 fi
@@ -89,7 +105,7 @@ if [ "$CONFIG" == "armv8" ];then
     echo '当前仓库最新内核版本：'$LATEST_KERNEL_VERSION
     cp -r $KERNEL_DIR/opt/kernel/$LATEST_KERNEL_VERSION/* $KERNEL_DIR/
     echo '开始进行打包'
-    package_firmware $PACKIT_DIR $ROOTFS_TAR_PATH $DEVICE $SCRIPT_DIR/whoami
+    package_firmware $PACKIT_DIR $OPENWRT_DIR/bin/targets/armvirt/64/openwrt-armvirt-64-default-rootfs.tar.gz $DEVICE $SCRIPT_DIR/whoami
     cd $PACKIT_DIR/output/
     [ ! -d "$OUTPUT_DIR" ] && mkdir -p $OUTPUT_DIR
     rm -rf $OUTPUT_DIR/*
@@ -97,6 +113,6 @@ if [ "$CONFIG" == "armv8" ];then
     7z a $OUTPUT_DIR/`ls *.img | head -1`.7z ./*.img
 else
     echo '打包固件中'
-    7z a $OUTPUT_DIR/$DEVICE'.bin.7z' $BUILD_TARGET_DIR/*/*.bin
+    7z a $OUTPUT_DIR/$DEVICE'.bin.7z' $OPENWRT_DIR/bin/targets/ramips/*/*.bin
 fi
 echo '编译固件成功：'${DEVICE}
