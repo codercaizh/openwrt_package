6242b413a6bc0305422a95862f906a3feb13c99c
codercaizh <545347837@qq.com>
2022-12-25 00:13:22 +0800

    fix: 修复n2n

--- scripts/after_update_feeds.sh
+++ scripts/after_update_feeds.sh
@@ -1,7 +1,7 @@
 # 执行脚本的目录在openwrt
 ## 更换n2n版本
-rm -rf package/lean/n2n/patches
 N2N_DIR=package/feeds/packages/n2n
+rm -rf $N2N_DIR/patches
 sed -i 's/PKG_VERSION:=3.0/PKG_VERSION:=3.1.1/g' $N2N_DIR/Makefile
 sed -i 's/^PKG_HASH:.*/PKG_HASH:=skip/g' $N2N_DIR/Makefile
 sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/ramips/Makefile
