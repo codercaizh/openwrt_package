8e1536b59d83c06aa34a27b338f7c8bedd6311db
codercaizh <545347837@qq.com>
2022-12-28 23:30:26 +0800

    内核升级到15版本

--- scripts/after_update_feeds.sh
+++ scripts/after_update_feeds.sh
@@ -4,7 +4,7 @@ N2N_DIR=package/feeds/packages/n2n
 rm -rf $N2N_DIR/patches
 sed -i 's/PKG_VERSION:=3.0/PKG_VERSION:=3.1.1/g' $N2N_DIR/Makefile
 sed -i 's/^PKG_HASH:.*/PKG_HASH:=skip/g' $N2N_DIR/Makefile
-sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/ramips/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.15/g' target/linux/ramips/Makefile
 # 以下是超頻
 sed -i 's/110,89/110,93/g' target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
 sed -i 's/cpu_clk, bus_clk;/cpu_clk, bus_clk, i;/g' target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
