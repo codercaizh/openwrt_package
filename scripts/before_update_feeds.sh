84429d7f8baddf5ca0006893fd5a4e0115738300
codercaizh <545347837@qq.com>
2023-01-16 12:11:53 +0800

    feat: 更换大吉为dnsgo

--- scripts/before_update_feeds.sh
+++ scripts/before_update_feeds.sh
@@ -1,3 +1,4 @@
 # 执行脚本的目录在openwrt
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git --depth=1 package/ddns-go
