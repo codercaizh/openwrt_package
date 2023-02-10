9c3f8617651f0b80f7de793e33d0bad6646bf4ad
codercai <545347837@qq.com>
2023-02-10 14:18:55 +0800

    Update after_update_feeds.sh
    取消升到5.15内核

--- scripts/after_update_feeds.sh
+++ scripts/after_update_feeds.sh
@@ -5,9 +5,9 @@ rm -rf $N2N_DIR/patches
 sed -i 's/PKG_VERSION:=3.0/PKG_VERSION:=3.1.1/g' $N2N_DIR/Makefile
 sed -i 's/^PKG_HASH:.*/PKG_HASH:=skip/g' $N2N_DIR/Makefile
 ## 更换mt6721为linux5.15内核版本
-sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.15/g' target/linux/ramips/Makefile
-sed -i 's/+1,5/+1,6/g' package/kernel/mt76/patches/010-bypass-werror.patch
-sed -i 's/+EXTRA_CFLAGS += -DCONFIG_MT76_LEDS/+EXTRA_CFLAGS += -DCONFIG_MT76_LEDS\n+KBUILD_CFLAGS += -Wno-implicit-int/g' package/kernel/mt76/patches/010-bypass-werror.patch
+# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.15/g' target/linux/ramips/Makefile
+# sed -i 's/+1,5/+1,6/g' package/kernel/mt76/patches/010-bypass-werror.patch
+# sed -i 's/+EXTRA_CFLAGS += -DCONFIG_MT76_LEDS/+EXTRA_CFLAGS += -DCONFIG_MT76_LEDS\n+KBUILD_CFLAGS += -Wno-implicit-int/g' package/kernel/mt76/patches/010-bypass-werror.patch
 ## 用于对mt6721平台超频
 sed -i 's/110,89/110,93/g' target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
 sed -i 's/cpu_clk, bus_clk;/cpu_clk, bus_clk, i;/g' target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
