9f35abaff2d81b79ee3738bc326ced545eaa13c6
codercaizh <545347837@qq.com>
2023-03-11 15:28:37 +0000

    增加新源测试

--- scripts/before_update_feeds.sh
+++ scripts/before_update_feeds.sh
@@ -1,4 +1,5 @@
 # 执行脚本的目录在openwrt
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git --depth=1 package/ddns-go
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+# git clone https://github.com/sirpdboy/luci-app-ddns-go.git --depth=1 package/ddns-go
