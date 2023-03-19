e4aa498de9fff9fea5ad122622b27a0428605be3
codercai <545347837@qq.com>
2023-03-19 09:22:24 +0800

    Update build_with_docker.sh
    解决无法看到编译过程问题

--- scripts/build_with_docker.sh
+++ scripts/build_with_docker.sh
@@ -112,7 +112,7 @@ check_complie_status
     check_complie_status
     if [ "$IS_COMPLIE" == "0" ]; then
         echo '第二次编译失败，启用低速编译排查问题'
-        make -j1
+        make V=s -j1
         check_complie_status
         if [ "$IS_COMPLIE" == "0" ]; then
             echo '最终编译失败，请根据日志排查原因'
