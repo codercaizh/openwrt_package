c7cd71601edb7a39813f742a122961220bfa40af
codercaizh <545347837@qq.com>
2023-04-09 12:40:27 +0000

    精简armv8

--- scripts/before_compile.sh
+++ scripts/before_compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 删除掉node依赖
 sed -i '/CONFIG_NODEJS/d' .config
-sed -i '/ddns-scripts/d' .config
+sed -i '/ddns/d' .config
 sed -i '/vlmcsd/d' .config
 sed -i '/ssr-plus/d' .config
 sed -i '/qbittorrent/d' .config
