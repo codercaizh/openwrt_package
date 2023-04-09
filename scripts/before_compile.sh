ddfcd6546e252afd032a27f5c52591edb9bd0b00
codercaizh <545347837@qq.com>
2023-04-09 17:36:55 +0000

    去掉ddns

--- scripts/before_compile.sh
+++ scripts/before_compile.sh
@@ -2,6 +2,7 @@
 # 删除掉node依赖
 sed -i '/CONFIG_NODEJS/d' .config
 sed -i '/-ddns/d' .config
+sed -i '/_ddns-/d' .config
 sed -i '/vlmcsd/d' .config
 sed -i '/ssr-plus/d' .config
 sed -i '/qbittorrent/d' .config
