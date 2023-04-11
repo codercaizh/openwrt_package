17ba87cb25cc44c7263b7d9bc3bea56cb08ab41b
codercaizh <545347837@qq.com>
2023-04-11 16:45:43 +0000

    路由固件内核切换为5.15

--- scripts/before_compile.sh
+++ scripts/before_compile.sh
@@ -7,9 +7,10 @@ sed -i 's/PKG_VERSION:=3.0/PKG_VERSION:=3.1.1/g' $N2N_DIR/Makefile
 sed -i 's/^PKG_HASH:.*/PKG_HASH:=skip/g' $N2N_DIR/Makefile
 
 ## 更换mt6721为linux5.15内核版本
-# sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.15/g' target/linux/ramips/Makefile
-# sed -i 's/+1,5/+1,6/g' package/kernel/mt76/patches/010-bypass-werror.patch
-# sed -i 's/+EXTRA_CFLAGS += -DCONFIG_MT76_LEDS/+EXTRA_CFLAGS += -DCONFIG_MT76_LEDS\n+KBUILD_CFLAGS += -Wno-implicit-int/g' package/kernel/mt76/patches/010-bypass-werror.patch
+sed -i 's/5_4/5_15/g' .config
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.15/g' target/linux/ramips/Makefile
+sed -i 's/+1,5/+1,6/g' package/kernel/mt76/patches/010-bypass-werror.patch
+sed -i 's/+EXTRA_CFLAGS += -DCONFIG_MT76_LEDS/+EXTRA_CFLAGS += -DCONFIG_MT76_LEDS\n+KBUILD_CFLAGS += -Wno-implicit-int/g' package/kernel/mt76/patches/010-bypass-werror.patch
 
 ## 用于对mt6721平台超频
 sed -i 's/110,89/110,93/g' target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
@@ -18,11 +19,3 @@ sed -i 's/pll = rt_memc_r32(MEMC_REG_CPU_PLL);/pll = rt_memc_r32(MEMC_REG_CPU_PL
 
 # 删除掉node依赖
 sed -i '/CONFIG_NODEJS/d' .config
-sed -i '/-ddns/d' .config
-sed -i '/_ddns-/d' .config
-sed -i '/vlmcsd/d' .config
-sed -i '/ssr-plus/d' .config
-sed -i '/qbittorrent/d' .config
-sed -i '/wol=/d' .config
-sed -i '/wol-/d' .config
-sed -i '/accesscontrol/d' .config
