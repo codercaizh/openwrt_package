d408260755338ae5b627fb5d3ee9dde48273db37
codercaizh <545347837@qq.com>
2023-04-17 15:44:55 +0000

    fix

--- scripts/before_update_feeds.sh
+++ scripts/before_update_feeds.sh
@@ -18,7 +18,6 @@ mv $SMALL_PACKAGE_TMP/luci-app-tencentddns $SMALL_PACKAGE_DIR/
 mv $SMALL_PACKAGE_TMP/luci-app-netspeedtest $SMALL_PACKAGE_DIR/
 mv $SMALL_PACKAGE_TMP/luci-app-wolplus $SMALL_PACKAGE_DIR/
 mv $SMALL_PACKAGE_TMP/homebox $SMALL_PACKAGE_DIR/
-mv $SMALL_PACKAGE_TMP/luci-app-beardropper  $SMALL_PACKAGE_DIR/
 mv $SMALL_PACKAGE_TMP/luci-app-bandwidthd  $SMALL_PACKAGE_DIR/
 mv $SMALL_PACKAGE_TMP/luci-app-netdata  $SMALL_PACKAGE_DIR/
 mv $SMALL_PACKAGE_TMP/luci-app-nginx-manager $SMALL_PACKAGE_DIR/
