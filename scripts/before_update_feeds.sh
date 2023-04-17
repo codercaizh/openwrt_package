2b10938d67db09dbe0b10e073bdd21d567e32d5c
codercai <545347837@qq.com>
2023-04-17 21:48:05 +0800

    Update before_update_feeds.sh

--- scripts/before_update_feeds.sh
+++ scripts/before_update_feeds.sh
@@ -19,5 +19,7 @@ mv $SMALL_PACKAGE_TMP/luci-app-netspeedtest $SMALL_PACKAGE_DIR/
 mv $SMALL_PACKAGE_TMP/luci-app-wolplus $SMALL_PACKAGE_DIR/
 mv $SMALL_PACKAGE_TMP/homebox $SMALL_PACKAGE_DIR/
 mv $SMALL_PACKAGE_TMP/luci-app-beardropper  $SMALL_PACKAGE_DIR/
+mv $SMALL_PACKAGE_TMP/luci-app-bandwidthd  $SMALL_PACKAGE_DIR/
+mv $SMALL_PACKAGE_TMP/luci-app-netdata  $SMALL_PACKAGE_DIR/
 mv $SMALL_PACKAGE_TMP/luci-app-nginx-manager $SMALL_PACKAGE_DIR/
 mv $SMALL_PACKAGE_TMP/.git $SMALL_PACKAGE_DIR/ && rm -rf $SMALL_PACKAGE_TMP
