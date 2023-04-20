478fda5926a2aba4c1eb84ec413f3aae16a5ef7a
codercai <545347837@qq.com>
2023-04-20 19:38:00 +0800

    Update before_update_feeds.sh

--- scripts/before_update_feeds.sh
+++ scripts/before_update_feeds.sh
@@ -11,6 +11,8 @@ function clondOrUpdateStore() {
         cd $PACKAGE_DIR/$STORE_NAME
         git reset --hard
         git fetch --all
+    else
+        git clone $GIT_URL $PACKAGE_DIR/$STORE_NAME
     fi
     git checkout $COMMIT_ID
     [ `echo "$COMMIT_ID"|awk '{print length($0)}'` != '40' ] && git pull # 如果是master分支则拉一下最新代码
