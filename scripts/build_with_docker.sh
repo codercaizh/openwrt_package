1a452d0cef621d8d954dbb9a9a3932eec694e5c2
codercaizh <545347837@qq.com>
2025-09-17 15:00:10 +0800

    fix

--- scripts/build_with_docker.sh
+++ scripts/build_with_docker.sh
@@ -101,7 +101,6 @@ if [[ $CONFIG == *armv8* ]];then
         wget "https://github.com/codercaizh/openwrt_package/releases/download/kernel_$KERNEL_TAG/$LATEST_KERNEL_VERSION" -q -P $KERNEL_DIR/
     fi
     7z x $KERNEL_DIR/$LATEST_KERNEL_VERSION -o$KERNEL_DIR/
-    rm -rf $KERNEL_DIR/$LATEST_KERNEL_VERSION
     KERNEL_VERSION=$(basename `ls -l $KERNEL_DIR/* | awk '{print $9}' | grep boot | head -1`)
     KERNEL_VERSION=${KERNEL_VERSION%%.tar.gz}
     KERNEL_VERSION=${KERNEL_VERSION##boot-}
