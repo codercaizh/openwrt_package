4343da3503652cb01a73a7c16c5273e2602738ea
codercai <545347837@qq.com>
2026-02-21 22:16:41 +0800

    Update before_update_feeds.sh

--- scripts/before_update_feeds.sh
+++ scripts/before_update_feeds.sh
@@ -18,7 +18,7 @@ function clondOrUpdateStore() {
 
 # 有新的feeds按照下面格式添加即可
 # clondOrUpdateStore "https://github.com/kenzok8/openwrt-packages" "kenzo" $OPENWRT_PACKAGES_COMMIT_ID
-clondOrUpdateStore "https://github.com/xiaorouji/openwrt-passwall" "passwall" $PASSWALL_PACKAGE_COMMIT_ID
+# clondOrUpdateStore "https://github.com/xiaorouji/openwrt-passwall" "passwall" $PASSWALL_PACKAGE_COMMIT_ID
 clondOrUpdateStore "https://github.com/kenzok8/small-package" "small-package" $SMALL_PACKAGE_COMMIT_ID
 # 添加自定义的部分源
 SMALL_PACKAGE_DIR=$PACKAGE_DIR/small-package;
