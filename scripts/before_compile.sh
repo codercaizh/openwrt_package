efed56a3091ab73ac8d343f52a2a135a03e33cfd
codercai <545347837@qq.com>
2026-02-25 00:24:47 +0800

    Update before_compile.sh

--- scripts/before_compile.sh
+++ scripts/before_compile.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 # 执行脚本的目录在openwrt
+RUST_DIR=package/feeds/packages/lang/rust
+sed -i 's/PKG_VERSION:=1.90.0/PKG_VERSION:=1.93.1/g' $RUST_DIR/Makefile
+sed -i 's/^PKG_HASH:.*/PKG_HASH:=skip/g' $RUST_DIR/Makefile
