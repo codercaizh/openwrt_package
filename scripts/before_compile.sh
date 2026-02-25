70d1cfff1ec36c33aafce602e5c9e735151d3a6d
codercai <545347837@qq.com>
2026-02-25 18:25:23 +0000

    解决流水线执行失败问题

--- scripts/before_compile.sh
+++ scripts/before_compile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # 执行脚本的目录在openwrt
 RUST_DIR=package/feeds/packages/rust
-sed -i 's/PKG_VERSION:=1.90.0/PKG_VERSION:=1.93.1/g' $RUST_DIR/Makefile
-sed -i 's/^PKG_HASH:.*/PKG_HASH:=skip/g' $RUST_DIR/Makefile
+if grep -qE '^[^#]*PKG_VERSION:=1.90.0' "$RUST_DIR/Makefile"; then
+    echo "rust版本有问题，使用fix版本替代"
+    cp $SCRIPT_DIR/fix_bugs/rust_Makefile $RUST_DIR/Makefile
+fi
+
