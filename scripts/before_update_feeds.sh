9e42484822a6dd5619fd9d1d4b96fa387b2065c6
codercai <545347837@qq.com>
2026-02-26 13:01:56 +0800

    Update before_update_feeds.sh

--- scripts/before_update_feeds.sh
+++ scripts/before_update_feeds.sh
@@ -22,13 +22,13 @@ clondOrUpdateStore "https://github.com/kenzok8/openwrt-packages" "kenzo"
 clondOrUpdateStore "https://github.com/kenzok8/small-package" "small-package" 
 clondOrUpdateStore "https://github.com/sirpdboy/luci-app-netspeedtest" "netspeedtest" 
 # 添加自定义的部分源
-SMALL_PACKAGE_DIR=$PACKAGE_DIR/small-package;
-SMALL_PACKAGE_TMP=/tmp/small-package
-mv $SMALL_PACKAGE_DIR $SMALL_PACKAGE_TMP && mkdir $SMALL_PACKAGE_DIR
-grep -E '^CONFIG_PACKAGE_luci-app-[^_]*=y$' "$CONFIG_DIR/$BUILD_CONFIG.config" \
-  | sed -E 's/^CONFIG_PACKAGE_(luci-app-[^=]+)=y$/\1/' \
-  | while IFS= read -r app; do
-      source="$SMALL_PACKAGE_TMP/$app"
-      [ -d "$source" ] && mv $source $SMALL_PACKAGE_DIR/ && echo "${app} 已安装" || echo "${app} 第三方库不存在"
-    done
-mv $SMALL_PACKAGE_TMP/.git $SMALL_PACKAGE_DIR/ && rm -rf $SMALL_PACKAGE_TMP
+# SMALL_PACKAGE_DIR=$PACKAGE_DIR/small-package;
+# SMALL_PACKAGE_TMP=/tmp/small-package
+# mv $SMALL_PACKAGE_DIR $SMALL_PACKAGE_TMP && mkdir $SMALL_PACKAGE_DIR
+# grep -E '^CONFIG_PACKAGE_luci-app-[^_]*=y$' "$CONFIG_DIR/$BUILD_CONFIG.config" \
+#  | sed -E 's/^CONFIG_PACKAGE_(luci-app-[^=]+)=y$/\1/' \
+#  | while IFS= read -r app; do
+#      source="$SMALL_PACKAGE_TMP/$app"
+#      [ -d "$source" ] && mv $source $SMALL_PACKAGE_DIR/ && echo "${app} 已安装" || echo "${app} 第三方库不存在"
+#    done
+# mv $SMALL_PACKAGE_TMP/.git $SMALL_PACKAGE_DIR/ && rm -rf $SMALL_PACKAGE_TMP
