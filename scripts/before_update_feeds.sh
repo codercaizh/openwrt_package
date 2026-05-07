8a280f9f587a430448645d50d05e84097fc942ca
codercai <545347837@qq.com>
2026-05-07 20:24:55 +0800

    Update before_update_feeds.sh

--- scripts/before_update_feeds.sh
+++ scripts/before_update_feeds.sh
@@ -22,7 +22,7 @@ clondOrUpdateStore "https://github.com/kenzok8/openwrt-packages" "kenzo"
 # 组播工具
 clondOrUpdateStore "https://github.com/stackia/rtp2httpd" "rtp2httpd"
 
-# cloudflarespeedtest
-clondOrUpdateStore "https://github.com/stevenjoezhang/luci-app-cloudflarespeedtest" "luci-app-cloudflarespeedtest"
+# small-package
+clondOrUpdateStore "https://github.com/kenzok8/small-package" "small-package"
 
 
